dc0adbf6c1d91c2e51b736cd59b38a583f06d558
root <root@a4s16.contrail.juniper.net>
2017-08-04 16:21:48 -0700

    Added init scripts

--- provisioning/cloud-init/init.sh
+++ provisioning/cloud-init/init.sh
@@ -3,3 +3,9 @@
 # Author: SAVITHRU LOKANATH
 # Contact: SAVITHRU AT JUNIPER.NET
 # Copyright (c) 2017 Juniper Networks, Inc. All rights reserved.
+
+sudo yum install epel-release vim NetworkManager -y
+sudo touch /tmp/hello-world
+sudo systemctl start NetworkManager && sudo systemctl enable NetworkManager
+#sudo yum update -y
+#sudo yum install ansible pyOpenSSL python-cryptography python-lxml git -y
