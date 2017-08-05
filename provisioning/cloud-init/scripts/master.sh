843bd79ed0444f63f742081638f0e618cef151b2
root <root@a4s16.contrail.juniper.net>
2017-08-05 00:21:37 -0700

    Added master/slave scripts

--- provisioning/cloud-init/scripts/master.sh
+++ provisioning/cloud-init/scripts/master.sh
@@ -20,5 +20,46 @@ wget http://10.84.5.120/github-build/R4.0/20/ubuntu-14-04/mitaka/artifacts_extra
 mkdir contrail-ansible && cd contrail-ansible
 cp /root/contrail-ansible-4.0.0.0-20.tar.gz . && tar -xvzf contrail-ansible-4.0.0.0-20.tar.gz
 cd /root/contrail-ansible/playbooks
-yes | cp /root/nested-k8s/contrail/all.yml inventory/my-inventory/group_vars
-yes | cp /root/nested-k8s/contrail/hosts inventory/my-inventory
+
+cat << EOF > inventory/my-inventory/hosts
+# Kubernetes Master-Node
+[contrail-repo]
+$master_ip
+
+# Contrail-Cloud Controller
+[kubernetes-contrail-controllers]
+$contrail_cfgm_ip
+
+# Contrail-Cloud Analytics
+[kubernetes-contrail-analytics]
+$contrail_analytics_ip
+
+# Kubernetes Master-Node
+[contrail-kubernetes]
+$master_ip
+
+# Kubernetes Slave-Node
+[contrail-compute]
+$slave_ip
+EOF
+
+cat << EOF > inventory/my-inventory/group_vars/all.yml
+docker_registry: $docker_registry:5000
+docker_registry_insecure: True
+docker_install_method: package
+docker_py_pkg_install_method: pip
+ansible_user: root
+ansible_become: true
+contrail_compute_mode: container
+os_release: $container_os
+contrail_version: $contrail_version
+cloud_orchestrator: $container_orchestrator
+vrouter_physical_interface: eth0
+webui_config: {http_listen_port: 8085}
+keystone_config: {ip: $openstack_keystone_ip, admin_password: $openstack_admin_pasword, admin_user: admin, admin_tenant: admin}
+nested_cluster_private_network: "10.10.10.0/24"
+kubernetes_cluster_name: nested-k8
+nested_cluster_network: {domain: $openstack_domain, project: $openstack_project, name: $openstack_public_network}
+nested_mode: true
+kubernetes_api_server: $master_ip
+EOF
